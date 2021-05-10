491ffbc686c94374abcdf8627da960b1e5d5b13d
vinczforte <83981529+vinczforte@users.noreply.github.com>
2021-05-10 22:12:37 +0300

    Add files via upload

---
+++ Autoriz.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp4.Pages
+{
+    public class UserInfo
+    {
+        public static int InfoRole
+        {
+            get;
+            set;
+        }
+    }
+    /// <summary>
+    /// Логика взаимодействия для Autoriz.xaml
+    /// </summary>
+    public partial class Autoriz : Page
+    {
+        public User AuthUser;
+        public Autoriz()
+        {
+            InitializeComponent();
+        }
+
+        private void Bautoriz_Click(object sender, RoutedEventArgs e)
+        {
+            var user = MainWindow.db.User.Where(x => x.Login == TBoxLogin.Text && x.Password == TBoxPass.Text).FirstOrDefault();
+            if (user != null)
+            {
+                if(user.Role == 1)
+                {
+                    MessageBox.Show("Главный");
+                    UserInfo.InfoRole = 1;
+                    this.NavigationService.Navigate(new MenuPage());
+                }
+                else
+                {
+                    if(user.Role == 2)
+                    {
+                        MessageBox.Show("Не главный");
+                        UserInfo.InfoRole = 2;
+                        this.NavigationService.Navigate(new MenuPage());
+                    }    
+                }
+            }    
+        }
+    }
+}
---
+++ ClientPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp4.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для ClientPage.xaml
+    /// </summary>
+    public partial class ClientPage : Page
+    {
+        public ClientPage()
+        {
+            InitializeComponent();
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new EditPage(new Client()));
+        }
+
+        private void DelButton_Click(object sender, RoutedEventArgs e)
+        {
+            var selClient = DgridView.SelectedItem as Client;
+            if (selClient != null)
+            {
+                MainWindow.db.Client.Remove(selClient);
+                MainWindow.db.SaveChanges();
+            }
+            else MessageBox.Show("Ничего не выбрано");
+            DgridView.ItemsSource = MainWindow.db.Client.ToList();
+        }
+
+        private void EditButton_Click(object sender, RoutedEventArgs e)
+        {
+            var SelClient = DgridView.SelectedItem as Client;
+            if (SelClient == null)
+            {
+                MessageBox.Show("Ничего не выбрано");
+                return;
+            }
+            else
+                NavigationService.Navigate(new EditPage(SelClient));
+        }
+
+        private void DgridView_Loaded(object sender, RoutedEventArgs e)
+        {
+            DgridView.ItemsSource = MainWindow.db.Client.ToList();
+        }
+    }
+}
